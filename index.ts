0a38b564df677d40b020387e0784e5129767e99d
Jayant Mathur <32935713+jayantmathur@users.noreply.github.com>
2026-03-30 11:32:43 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -556,6 +556,16 @@ async function main() {
                     dev: 'quarto preview index.qmd',
                     render: 'quarto render',
                   },
+                  "prettier": {
+        "overrides": [
+            {
+                "files": "*.qmd",
+                "options": {
+                    "parser": "markdown"
+                }
+            }
+        ]
+    }
                 }),
               )
 
